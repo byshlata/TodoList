04fccf0ef664ec669b00a6ce562367d781c5d67d
byshlata <70332441+byshlata@users.noreply.github.com>
2022-06-16 14:17:13 +0300

    add Page404.module.css and Page404.tsx

---
+++ src/component/page404/Page404.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from 'react';
+
+import s from './Page404.module.css';
+
+export const Page404 = (): ReactElement => (
+  <div className={s.page404}>404: PAGE NOT FOUND</div>
+);
